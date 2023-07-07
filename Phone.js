b1f9c1b5053f3f990bc7201455e5c30cdf3d8371
Samuel2228 <sammycj22@gmail.com>
2023-07-07 08:44:16 +0100

    UPDATE

--- Phone.js
+++ Phone.js
@@ -3,14 +3,14 @@ let jambScore = document.getElementById('jamb-score');
 let budgetApp = document.getElementById('budget-app');
 
 readBtn.addEventListener('click', ()=> {
-    let todoMessage = 'Todo App is an app that I have been praticing and learing since my career as a developer with HTML & CSS. I created this Todo-App recently with my little knowledge of javascript. ';
+    let todoMessage = 'Todo App is an app that I have been praticing when I learned how to create it with HTML & CSS. I created this Todo-App recently with my little knowledge of javascript. ';
     alert(`Todo App\n \n ${ todoMessage}`)
 })
 
 // Jamb score 
 
 jambScore.addEventListener('click', ()=> {
-    let jambMsg = "\n My first project in javascript. Jamb Score is an app use to check student score I've created 3 different Jamb Score.";
+    let jambMsg = "\n My first project in javascript. Jamb Score It's an app use to check student's scores I've created 3 different Jamb Score.";
      alert(`Jamb Score\n ${jambMsg}`)
 })
 
